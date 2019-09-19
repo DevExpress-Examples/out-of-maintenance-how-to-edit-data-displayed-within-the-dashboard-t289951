367a6591b7be5af3a48d20576cbdd37b107b21a5
DevExpressExampleBot <fakeEmail>
2019-09-19 11:35:57 +0300

    Source auto update [skip ci]

--- VB/Dashboard_EditData/DashboardViewerForm.vb
+++ VB/Dashboard_EditData/DashboardViewerForm.vb
@@ -5,39 +5,43 @@ Imports DevExpress.XtraBars.Ribbon
 Imports DevExpress.XtraBars
 
 Namespace Dashboard_EditData
-	Partial Public Class DashboardViewerForm
-		Inherits RibbonForm
+    Partial Public Class DashboardViewerForm
+        Inherits RibbonForm
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub dashboardViewer1_DashboardItemClick(ByVal sender As Object, ByVal e As DashboardItemMouseActionEventArgs) Handles dashboardViewer1.DashboardItemClick
-			If e.DashboardItemName = "gridDashboardItem1" And e.GetUnderlyingData() IsNot Nothing Then
-				ShowUnderlyingData(e.GetUnderlyingData())
-			End If
-		End Sub
+        Private Sub dashboardViewer1_DashboardItemClick(ByVal sender As Object, _
+                                                        ByVal e As DashboardItemMouseActionEventArgs) _
+                                                    Handles dashboardViewer1.DashboardItemClick
+            If e.DashboardItemName = "gridDashboardItem1" And e.GetUnderlyingData() IsNot Nothing Then
+                ShowUnderlyingData(e.GetUnderlyingData())
+            End If
+        End Sub
 
-		Private Sub barButtonItem1_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs) Handles barButtonItem1.ItemClick
-			ShowUnderlyingData(dashboardViewer1.GetUnderlyingData("gridDashboardItem1"))
-		End Sub
+        Private Sub barButtonItem1_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs) _
+            Handles barButtonItem1.ItemClick
+            ShowUnderlyingData(dashboardViewer1.GetUnderlyingData("gridDashboardItem1"))
+        End Sub
 
-		Private Sub barButtonItem2_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs) Handles barButtonItem2.ItemClick
-			dashboardViewer1.ReloadData()
-		End Sub
+        Private Sub barButtonItem2_ItemClick(ByVal sender As Object, ByVal e As ItemClickEventArgs) _
+            Handles barButtonItem2.ItemClick
+            dashboardViewer1.ReloadData()
+        End Sub
 
-		Private Sub ShowUnderlyingData(ByVal unData As DashboardUnderlyingDataSet)
-			Dim form As New UnderlyingDataForm()
-			Dim keyColumnName As String = "OrderID"
+        Private Sub ShowUnderlyingData(ByVal unData As DashboardUnderlyingDataSet)
+            Dim form As New UnderlyingDataForm()
+            Dim keyColumnName As String = "OrderID"
 
-			Dim filterList As New List(Of Object)()
-			For Each row As DashboardUnderlyingDataRow In unData
-				filterList.Add(row(keyColumnName))
-			Next row
-			form.SetFilter(keyColumnName, filterList)
+            Dim filterList As New List(Of Object)()
+            For Each row As DashboardUnderlyingDataRow In unData
+                filterList.Add(row(keyColumnName))
+            Next row
+            form.SetFilter(keyColumnName, filterList)
 
-			form.ShowDialog()
-			form.Dispose()
-		End Sub
-	End Class
+            form.ShowDialog()
+            form.Dispose()
+        End Sub
+    End Class
 End Namespace
--- VB/Dashboard_EditData/UnderlyingDataForm.vb
+++ VB/Dashboard_EditData/UnderlyingDataForm.vb
@@ -7,29 +7,29 @@ Imports DevExpress.XtraEditors
 Imports DevExpress.XtraGrid.Views.Base
 
 Namespace Dashboard_EditData
-	Partial Public Class UnderlyingDataForm
-		Inherits DevExpress.XtraEditors.XtraForm
+    Partial Public Class UnderlyingDataForm
+        Inherits DevExpress.XtraEditors.XtraForm
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub UnderlyingDataForm_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-			Me.ordersTableAdapter.Fill(Me.nwindDataSet.Orders)
-		End Sub
+        Private Sub UnderlyingDataForm_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+            Me.ordersTableAdapter.Fill(Me.nwindDataSet.Orders)
+        End Sub
 
-		Private Sub gridView1_RowUpdated(ByVal sender As Object, ByVal e As RowObjectEventArgs) Handles gridView1.RowUpdated
-			Try
-				Me.ordersTableAdapter.Update(Me.nwindDataSet.Orders)
-			Catch
-				XtraMessageBox.Show("Update failed")
-			End Try
-		End Sub
+        Private Sub gridView1_RowUpdated(ByVal sender As Object, ByVal e As RowObjectEventArgs) Handles gridView1.RowUpdated
+            Try
+                Me.ordersTableAdapter.Update(Me.nwindDataSet.Orders)
+            Catch
+                XtraMessageBox.Show("Update failed")
+            End Try
+        End Sub
 
-		Public Sub SetFilter(ByVal columnName As String, ByVal filterList As List(Of Object))
-			Dim orderIdColumn As GridColumn = CType(Me.gridControl1.DefaultView, GridView).Columns(columnName)
-			Dim filterInfo As New ColumnFilterInfo(New InOperator(columnName, filterList))
-			orderIdColumn.FilterInfo = filterInfo
-		End Sub
-	End Class
+        Public Sub SetFilter(ByVal columnName As String, ByVal filterList As List(Of Object))
+            Dim orderIdColumn As GridColumn = CType(Me.gridControl1.DefaultView, GridView).Columns(columnName)
+            Dim filterInfo As New ColumnFilterInfo(New InOperator(columnName, filterList))
+            orderIdColumn.FilterInfo = filterInfo
+        End Sub
+    End Class
 End Namespace
